8fb186c0f217d71fbed547e218a00771cdb60aca
Baige He <baige_he@163.com>
2024-10-24 15:28:21 -0700

    Create MenuItemReviewController, add GET and POST. Modified Repository

--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.MenuItemReview;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface  MenuItemReviewRepository extends CrudRepository<UCSBDate, Long> {
+public interface  MenuItemReviewRepository extends CrudRepository<MenuItemReview, Long> {
   /**
    * This method returns all UCSBDate entities with a given quarterYYYYQ.
    * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for Spring 2024, 20243 for Summer 2024, 20244 for Fall 2024)
    * @return all UCSBDate entities with a given quarterYYYYQ
    */
-  Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
+  // Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
 }
